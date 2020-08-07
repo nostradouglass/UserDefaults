6f53348c53fb067ba9f85375222292160129916f
Kelly Douglass <kdouglass@gmail.com>
2020-08-07 10:47:22 -0500

    Initial Commit

---
+++ UserDefaults1/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  UserDefaults1
+//
+//  Created by Kelly Douglass on 8/7/20.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
